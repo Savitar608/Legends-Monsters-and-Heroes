b95b330d564b0e61f2285fc9843a156a496515a4
Adithya <adithya608@gmail.com>
2025-11-20 22:33:26 -0500

    Refactor build script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -r bin && mkdir -p bin
-javac -d bin -sourcepath src/main/java src/main/java/com/legends/Main.java
+mkdir -p bin
+javac -d bin @sources.txt
 cp -r src/main/resources/* bin/
 java -cp bin com.legends.Main
