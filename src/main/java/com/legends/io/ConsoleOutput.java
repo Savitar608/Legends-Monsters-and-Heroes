684d4c4ead3556ab20db73f7068a730f1b7b28cc
Savitar608 <adithya608@gmail.com>
2025-11-25 13:30:22 -0500

    Add ANSI color constants and methods for colored console output

--- src/main/java/com/legends/io/ConsoleOutput.java
+++ src/main/java/com/legends/io/ConsoleOutput.java
@@ -1,6 +1,10 @@
 package com.legends.io;
 
 public class ConsoleOutput implements Output {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+
     @Override
     public void print(Object s) {
         System.out.print(s);
@@ -19,5 +23,15 @@ public class ConsoleOutput implements Output {
     @Override
     public void printError(Object s) {
         System.err.println(s);
+    }
+
+    @Override
+    public void printlnGreen(Object s) {
+        System.out.println(ANSI_GREEN + s + ANSI_RESET);
+    }
+
+    @Override
+    public void printlnRed(Object s) {
+        System.out.println(ANSI_RED + s + ANSI_RESET);
     }
 }
